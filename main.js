a6a76686b11a7eab8f1bc4c77df56349fcf3d9cb
Aya74 <alarayshi.aya2002@gmail.com>
2022-05-12 19:02:38 +0300

    Create data var to catch tablr body relates #5

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
+//inputs var
+var courseNameInput = document.getElementById("courseName");
+var courseCategoryInput = document.getElementById("courseCategory");
+var coursePriceInput = document.getElementById("coursePrice");
+var courseDescriptionInput = document.getElementById("courseDescription");
+//butyons var
+var addBtn = document.getElementById("click");
+//Array to save objects
+var courses = [];
+//var to catch table body
+var data = document.getElementById("data");
 
-var courseNameInput = document.getElementById('courseName');
-var courseCategoryInput = document.getElementById('courseCategory');
-var coursePriceInput = document.getElementById('coursePrice');
-var courseDescriptionInput = document.getElementById('courseDescription');
+// create function add course to add course on click button Add course
+addBtn.onclick = function () {
+  //create course object to get element from user inputs
+  var course = {
+    name: courseNameInput.value,
+    category: courseCategoryInput.value,
+    price: coursePriceInput.value,
+    description: courseDescriptionInput.value,
+  };
+  //Add object course to array courses
+  courses.push(course);
+};
