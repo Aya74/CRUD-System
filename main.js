5da399b2545af99995f67281e278da1cde7d4f6c
Aya74 <alarayshi.aya2002@gmail.com>
2022-05-12 18:19:40 +0300

    Create var to catch the input using id relates #5

---
+++ main.js
@@ -0,0 +1,5 @@
+
+var courseNameInput = document.getElementById('courseName');
+var courseCategoryInput = document.getElementById('courseCategory');
+var coursePriceInput = document.getElementById('coursePrice');
+var courseDescriptionInput = document.getElementById('courseDescription');
