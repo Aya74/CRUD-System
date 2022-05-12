6df923ad098f1340d18e4e5b8e48a1ae51af5562
Aya74 <alarayshi.aya2002@gmail.com>
2022-05-12 20:52:37 +0300

    Create 2 function, add course function to add course in an object of array, display data function to display course information relates #5

--- main.js
+++ main.js
@@ -10,8 +10,13 @@ var courses = [];
 //var to catch table body
 var data = document.getElementById("data");
 
-// create function add course to add course on click button Add course
 addBtn.onclick = function () {
+  addCourse();
+  displayData();
+};
+
+// create function add course to add course on click button Add course
+function addCourse() {
   //create course object to get element from user inputs
   var course = {
     name: courseNameInput.value,
@@ -21,4 +26,23 @@ addBtn.onclick = function () {
   };
   //Add object course to array courses
   courses.push(course);
-};
+}
+
+// create function display data on click button Add course
+function displayData() {
+  //Add course to the body of the table
+  var result = "";
+  for (i = 0; i < courses.length; i++) {
+    result += `
+    <tr>
+      <td>${i + 1}</td>
+      <td>${courses[i].name}</td>
+      <td>${courses[i].category}</td>
+      <td>${courses[i].price}</td>
+      <td>${courses[i].description}</td>
+      <td></td>
+      <td></td>
+      </tr>`;
+  }
+  data.innerHTML = result;
+}
